643c0ab7cf69d8ddc28cb58079a07e995ae7419b
valentioaditama <valentioaditama8@gmail.com>
2023-03-04 08:40:28 +0700

    Update

---
+++ src/views/auth/register/Register.vue
@@ -0,0 +1,34 @@
+<style>
+@import './Register.css';
+</style>
+
+<template>
+    <div class="container">
+        <div class="row contentCenter">
+            <div class="col-md-6">
+                <img src="../../../assets/login.gif" class="img-fluid" alt="image">
+            </div>
+            <div class="col-md-6">
+                <form>
+                    <div class="mb-3">
+                        <h3>Register</h3>
+                        <h6 class="subTitle">Create your first account</h6>
+                    </div>
+                    <div class="mb-3">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password">
+                    </div>
+                    <div class="mb-3">
+                        <RouterLink to="/">Have Account?</RouterLink>
+                    </div>
+                    <button type="submit" class="btn btn-primary container">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
